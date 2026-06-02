67b8036c9743df242854a47dba2f3a82127761d5
Wolphen <enzo7825@gmail.com>
2026-06-02 12:22:11 +0200

    pushing to deploy

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-// 1. IMPORTATION DE TES WIDGETS
-// (Vérifie bien que le chemin correspond à ton projet, ex: 'package:ton_projet/widgets/...')
 import '../widgets/landing/hero_section.dart';
 import '../widgets/landing/features_section.dart';
 import '../widgets/landing/screenshots_section.dart';
@@ -19,14 +17,14 @@ class LandingPage extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            // 2. REMPLACEMENT DES CONTAINERS PAR TES SECTIONS CLEAN
-            HeroSection(),         // Contient ton titre CaddieScan et le bouton Admin
-            FeaturesSection(),     // Vos fonctionnalités
-            ScreenshotsSection(),  // Vos captures d'écran mobiles
-            PricingSection(),      // Vos tarifs / formules
-            FaqSection(),          // Vos questions fréquentes
-            ContactSection(),      // Votre formulaire de contact
-            FooterSection(),       // Votre bas de page avec le copyright
+        
+            HeroSection(),         
+            FeaturesSection(),     
+            ScreenshotsSection(),  
+            PricingSection(),      
+            FaqSection(),          
+            ContactSection(),      
+            FooterSection(),       
           ],
         ),
       ),
